b1bb4cda4daa783d7bb8775679497eb03eb1583c
Mathias Hansen <me@codemonkey.io>
2025-02-09 10:41:49 -0500

    fix: Syntax

--- openaddr/conform.py
+++ openaddr/conform.py
@@ -606,7 +606,7 @@ def ogr_source_to_csv(source_config, source_path, dest_path, disable_centroids):
             if geom is not None:
                 geom.Transform(coordTransform)
 
-                if source_config.layer == "addresses" and !disable_centroids:
+                if source_config.layer == "addresses" and not disable_centroids:
                     # For Addresses - Calculate the centroid on surface of the geometry and write it as X and Y columns
                     try:
                         centroid = geom.PointOnSurface()
@@ -736,7 +736,7 @@ def geojson_source_to_csv(source_config, source_path, dest_path, disable_centroi
                     if not geom:
                         continue
 
-                    if source_config.layer == "addresses" and !disable_centroids:
+                    if source_config.layer == "addresses" and not disable_centroids:
                         # For Addresses - Calculate the centroid on surface of the geometry and write it as X and Y columns
                         geom = geom.PointOnSurface()
 
@@ -834,7 +834,7 @@ def row_extract_and_reproject(source_config, source_row, disable_centroids):
                 _L.debug("Could not reproject %s %s in SRS %s", source_x, source_y, srs)
 
     # For Addresses - Calculate the centroid on surface of the geometry and write it as X and Y columns
-    if source_config.layer == "addresses" and !disable_centroids:
+    if source_config.layer == "addresses" and not disable_centroids:
         geom = ogr.CreateGeometryFromWkt(source_geom)
 
         try:
